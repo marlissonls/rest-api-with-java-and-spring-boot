b8c8cfa9e67e52cff2e6f64a6df66eed721d925c
Marlisson Silva <marlisson.business@gmail.com>
2026-03-06 15:31:17 -0300

    feat: add Flyway CLI support for database migrations

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+    (1, 'Georgia - EUA', 'Mike', 'MALE', 'Tyson'),
+    (2, 'Mvezo - South Africa', 'Nelson', 'MALE', 'Mandela'),
+    (3, 'Mvezo - South Africa', 'Gavin', 'MALE', 'Schmidth');
