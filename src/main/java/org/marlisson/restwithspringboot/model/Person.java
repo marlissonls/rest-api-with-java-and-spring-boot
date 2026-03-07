ae7b791c2a5c3380a96390fd36a8a3410ac14f92
Marlisson Silva <marlisson.business@gmail.com>
2026-03-07 18:05:42 -0300

    fix: fix incorrect @Column annotations in Person entity

--- src/main/java/org/marlisson/restwithspringboot/model/Person.java
+++ src/main/java/org/marlisson/restwithspringboot/model/Person.java
@@ -18,16 +18,16 @@ public class Person implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "first_name, nullable = false, length = 80")
+    @Column(name = "first_name", nullable = false, length = 80)
     private String firstName;
 
-    @Column(name = "last_name, nullable = false, length = 80")
+    @Column(name = "last_name", nullable = false, length = 80)
     private String lastName;
 
-    @Column(name = "nullable = false, length = 100")
+    @Column(nullable = false, length = 100)
     private String address;
 
-    @Column(name = "nullable = false, length = 6")
+    @Column(nullable = false, length = 6)
     private String gender;
 
     public Person() {
