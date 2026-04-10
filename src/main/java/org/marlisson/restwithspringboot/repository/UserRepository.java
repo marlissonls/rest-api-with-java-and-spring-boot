b4ea93c53fc8e4afc631d1e0c3ec61a96647e4dd
Marlisson Silva <marlisson.business@gmail.com>
2026-04-10 20:02:32 -0300

    feat: add findByUsername query to UserRepository

---
+++ src/main/java/org/marlisson/restwithspringboot/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package org.marlisson.restwithspringboot.repository;
+
+import org.marlisson.restwithspringboot.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE u.userName =:userName")
+    User findByUsername(@Param("userName") String userName);
+
+}
